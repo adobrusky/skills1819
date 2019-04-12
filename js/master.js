25d0fc68b811880662d8a9b9239b6010adfb60ba
thefierytortoise <austindobrusky@gmail.com>
2019-04-12 12:18:19 -0500

    df

--- js/master.js
+++ js/master.js
@@ -1,18 +1,21 @@
 let pageScroll;
 function navbarChange() {
-  if(pageScroll > navbarHeight && screenWidth > small) {
+  if(pageScroll > 0 || screenWidth < medium) {
     $('.nav, .nav-items').css('backgroundColor', '#353535');
-  } else {
+  } else if(pageScroll === 0 && screenWidth > medium){
     $('.nav, .nav-items').css('backgroundColor', 'rgba(0, 0, 0, 0.3)');
+    console.log(navbarHeight);
   }
+  console.log(screenWidth);
 }
-
 
 $(document).ready(function() {
   navbarChange();
-
-  $(window).scroll() {
+  $(window).scroll(function() {
     pageScroll = $(window).scrollTop();
     navbarChange();
-  }
+  })
+  $(window).resize(function() {
+        navbarChange();
+  })
 });
